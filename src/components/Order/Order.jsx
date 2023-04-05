46ef262ab9f76b0627ecb656e5cb928eda4efb2a
Ahashan Habib Utsho <utsho926@gmail.com>
2023-04-05 17:39:46 +0600

    order page functionality added

--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -4,23 +4,32 @@ import { useLoaderData } from 'react-router-dom';
 import OrderReview from '../OrderReview/OrderReview';
 
 const Order = () => {
-    let products = useLoaderData()
-    let [storedProducts, setStoredProducts] = useState(products)
+    let storedCart = useLoaderData()
+    let [cart, setCart] = useState(storedCart)
 
     // delete product functionality
-    function deleteItems(product){
-        let deleteProduct = storedProducts.filter(dltPd => dltPd.id !== product.id)
-        setStoredProducts(deleteProduct)
+    function deleteItems(product) {
+        // delete items from cart
+        let deleteProduct = cart.filter(dltPd => dltPd.id !== product.id)
+        setCart(deleteProduct)
+
+        // delete items from local storage
+        let localStorageData = JSON.parse(localStorage.getItem('cartItem'))
+        if (product.id in localStorageData) {
+            delete localStorageData[product.id]
+        }
+        localStorage.setItem('cartItem', JSON.stringify(localStorageData))
     }
+
     return (
         <section className='grid grid-cols-12 py-20 gap-14 max-w-7xl mx-10 xl:mx-auto'>
             <div className='col-span-8 px-14 space-y-5'>
                 {
-                    storedProducts.map(product => <OrderReview deleteItems={deleteItems} key={product.id} product={product} />)
+                    cart.map(product => <OrderReview deleteItems={deleteItems} key={product.id} product={product} />)
                 }
             </div>
             <div className='col-span-4 sticky top-2 self-baseline bg-orange-200 rounded-lg'>
-                <Cart cart={storedProducts} clearCartFunc={()=>setStoredProducts([])}></Cart>
+                <Cart cart={cart} clearCartFunc={() => setCart([])}></Cart>
             </div>
         </section>
     );
